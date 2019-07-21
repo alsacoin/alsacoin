c634d542abce5d83da59a7c91c8483acae3f535b
chritchens <chritchens@gmail.com>
2019-07-21 18:58:25 +0200

    Update the difficulty mechanism

--- mining/src/difficulty.rs
+++ mining/src/difficulty.rs
@@ -3,39 +3,42 @@
 //! The `difficulty` module contains the difficulty calculation functions.
 
 use crate::common::riemmann_zeta_2;
+use crate::error::Error;
 use crate::result::Result;
-use std::f64::consts::PI;
-
-/// `calc_k` calculates 6/pi^2.
-pub fn calc_k() -> f64 {
-    6.0 / (PI.powi(2))
-}
 
 /// `difficulty` calculates the difficulty bits given a specific distance
-/// from the eve transaction.
-pub fn difficulty(d: u64) -> Result<u64> {
-    let k = calc_k();
-    let res = (512f64 * k * riemmann_zeta_2(d)?).floor() as u64;
+/// from the eve transaction and a specific amount.
+pub fn difficulty(d: u64, a: u64) -> Result<u64> {
+    if (d == 0) || (a == 0) {
+        let err = Error::OutOfBound;
+        return Err(err);
+    }
+
+    let d = 1 + (d as f64 / 1000f64) as u64;
+    let a = 1 + (a as f64 / 1000f64) as u64;
+    let res = (64f64 * riemmann_zeta_2(d)? / riemmann_zeta_2(a)?).floor() as u64;
     Ok(res)
 }
 
 #[test]
 fn test_difficulty() {
-    let ds = [1, 10, 100, 1000, 1_000_000];
-    let expected = [311, 482, 508, 511, 511];
+    let ds = [1, 1_000, 1_000_000];
+    let ams = [1, 1_000, 1_000_000];
+    let expected = [[64, 51, 38], [80, 64, 48], [105, 84, 64]];
 
-    let res = difficulty(0);
+    let res = difficulty(0, 1);
+    assert!(res.is_err());
+
+    let res = difficulty(1, 0);
     assert!(res.is_err());
 
     for (i, d) in ds.iter().enumerate() {
-        let res = riemmann_zeta_2(*d);
-        assert!(res.is_ok());
+        for (j, a) in ams.iter().enumerate() {
+            let res = difficulty(*d, *a);
+            assert!(res.is_ok());
 
-        let res = difficulty(*d);
-        assert!(res.is_ok());
-
-        let diff = res.unwrap();
-
-        assert_eq!(diff, expected[i]);
+            let diff = res.unwrap();
+            assert_eq!(diff, expected[i][j]);
+        }
     }
 }
