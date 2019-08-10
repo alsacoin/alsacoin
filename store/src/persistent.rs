273c7a642e96313eb917a7f9bda4145871bdf20e
chritchens <chritchens@gmail.com>
2019-08-10 12:54:49 +0200

    Add helper methods

--- store/src/persistent.rs
+++ store/src/persistent.rs
@@ -2,10 +2,11 @@
 //
 // `persistent` contains the persistent store type and functions.
 
+use crate::error::Error;
 use crate::result::Result;
 use crate::traits::Store;
 use futures::future::BoxFuture;
-use rkv::{Manager, Rkv, SingleStore, StoreOptions};
+use rkv::{Manager, Rkv, SingleStore, StoreOptions, Value};
 use std::path::{Path, PathBuf};
 use std::sync::{Arc, RwLock};
 
@@ -53,6 +54,74 @@ impl PersistentStore {
     pub fn update_size(&mut self) -> Result<()> {
         // TODO
         unreachable!()
+    }
+
+    /// `get` gets a key-value pair from the `PersistentStore`.
+    pub fn get(&self, name: &str, key: &[u8]) -> Result<Vec<u8>> {
+        let env = self.manager.read()?;
+        let reader = env.read()?;
+        if let Some(value) = self.open(name)?.get(&reader, key)? {
+            value.to_bytes().map_err(|e| e.into())
+        } else {
+            let err = Error::NotFound;
+            Err(err)
+        }
+    }
+
+    /// `iter` returns the `PersistentStore` iterator over the key-value pairs.
+    pub fn iter(
+        &self,
+        name: &str,
+        from: &[u8],
+        to: &[u8],
+        count: u32,
+        skip: u32,
+    ) -> Result<Vec<Vec<u8>>> {
+        let env = self.manager.read()?;
+        let reader = env.read()?;
+        let mut values = Vec::new();
+        let mut skipped = 0;
+        let mut counted = 0;
+
+        for res in self.open(name)?.iter_start(&reader)? {
+            let (k, v) = res?;
+
+            if (from <= k) && (to > k) {
+                if skipped >= skip {
+                    if counted <= count {
+                        if let Some(value) = v {
+                            values.push(value.to_bytes()?);
+                            counted += 1;
+                        } else {
+                            break;
+                        }
+                    } else {
+                        break;
+                    }
+                }
+
+                skipped += 1;
+            }
+        }
+
+        Ok(values)
+    }
+
+    /// `put` puts a binary key-value pair in the `PersistentStore`.
+    pub fn put(&mut self, name: &str, key: &[u8], value: &[u8]) -> Result<()> {
+        let env = self.manager.read()?;
+        let mut writer = env.write()?;
+        self.open(name)?
+            .put(&mut writer, key, &Value::Blob(value))?;
+        writer.commit().map_err(|e| e.into())
+    }
+
+    /// `delete` deletes a key-value pair from the `PersistentStore`.
+    pub fn delete(&mut self, name: &str, key: &[u8]) -> Result<()> {
+        let env = self.manager.read()?;
+        let mut writer = env.write()?;
+        self.open(name)?.delete(&mut writer, key)?;
+        writer.commit().map_err(|e| e.into())
     }
 
     /// `clear` clears the `PersistentStore`.
