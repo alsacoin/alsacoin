d8911f1402d7af95bd56dfc0a54ace1fe078d48e
chritchens <chritchens@gmail.com>
2019-08-29 10:03:15 +0200

    Remove unused dependencies

--- config/src/error.rs
+++ config/src/error.rs
@@ -2,8 +2,6 @@
 //!
 //! `error` contains the `config` crate `Error` type.
 
-use crypto::error::Error as CryptoError;
-use models::error::Error as ModelError;
 use serde_cbor;
 use serde_json;
 use std::convert::From;
@@ -13,10 +11,6 @@ use std::io;
 pub enum Error {
     #[fail(display = "IO: {}", msg)]
     IO { msg: String },
-    #[fail(display = "Crypto: {}", msg)]
-    Crypto { msg: String },
-    #[fail(display = "Model: {}", msg)]
-    Model { msg: String },
     #[fail(display = "Parse: {}", msg)]
     Parse { msg: String },
     #[fail(display = "Not implemented")]
@@ -51,19 +45,5 @@ impl From<serde_json::error::Error> for Error {
     fn from(err: serde_json::error::Error) -> Error {
         let msg = format!("{}", err);
         Error::Parse { msg }
-    }
-}
-
-impl From<CryptoError> for Error {
-    fn from(error: CryptoError) -> Error {
-        let msg = format!("{}", error);
-        Error::Crypto { msg }
-    }
-}
-
-impl From<ModelError> for Error {
-    fn from(error: ModelError) -> Error {
-        let msg = format!("{}", error);
-        Error::Model { msg }
     }
 }
