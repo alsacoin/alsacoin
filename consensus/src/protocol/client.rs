0e85c2cd6a807f7763bf11cc6429705178b6b90e
chritchens <chritchens@gmail.com>
2019-09-06 12:58:42 +0200

    Implement protocol client

--- consensus/src/protocol/client.rs
+++ consensus/src/protocol/client.rs
@@ -1,3 +1,151 @@
 //! # Protocol Client
 //!
 //! `client` is the module containing the protocol client type and functions.
+
+use crate::protocol::network as protocol_network;
+use crate::protocol::ProtocolState;
+use crate::result::Result;
+use crypto::hash::Digest;
+use models::node::Node;
+use models::transaction::Transaction;
+use network::traits::Transport;
+use std::collections::BTreeSet;
+use std::sync::{Arc, Mutex};
+use store::traits::Store;
+
+/// `ProtocolClient` is the protocol client type.
+pub struct ProtocolClient<S: Store, P: Store, T: Transport> {
+    pub state: Arc<Mutex<ProtocolState<S, P>>>,
+    pub transport: Arc<Mutex<T>>,
+}
+
+impl<S: Store, P: Store, T: Transport> ProtocolClient<S, P, T> {
+    /// `new` creates a new `ProtocolClient`.
+    pub fn new(
+        state: Arc<Mutex<ProtocolState<S, P>>>,
+        transport: Arc<Mutex<T>>,
+    ) -> Result<ProtocolClient<S, P, T>> {
+        state.lock().unwrap().validate()?;
+
+        let client = ProtocolClient { state, transport };
+
+        Ok(client)
+    }
+
+    /// `validate` validates the `ProtocolClient`.
+    pub fn validate(&self) -> Result<()> {
+        self.state.lock().unwrap().validate()
+    }
+
+    /// `fetch_node_transactions` fetches transactions from a remote node.
+    pub fn fetch_node_transactions(
+        &mut self,
+        address: &[u8],
+        ids: &BTreeSet<Digest>,
+    ) -> Result<BTreeSet<Transaction>> {
+        protocol_network::fetch_node_transactions(
+            self.state.clone(),
+            self.transport.clone(),
+            address,
+            ids,
+        )
+    }
+
+    /// `fetch_transactions` fetches transactions from remote.
+    pub fn fetch_transactions(&mut self, ids: &BTreeSet<Digest>) -> Result<BTreeSet<Transaction>> {
+        protocol_network::fetch_transactions(self.state.clone(), self.transport.clone(), ids)
+    }
+
+    /// `fetch_node_random_transactions` fetches random transactions from a remote node.
+    pub fn fetch_node_random_transactions(
+        &mut self,
+        address: &[u8],
+        count: u32,
+    ) -> Result<BTreeSet<Transaction>> {
+        protocol_network::fetch_node_random_transactions(
+            self.state.clone(),
+            self.transport.clone(),
+            address,
+            count,
+        )
+    }
+
+    /// `fetch_random_transactions` fetches random transactions from remote.
+    pub fn fetch_random_transactions(&mut self, count: u32) -> Result<BTreeSet<Transaction>> {
+        protocol_network::fetch_random_transactions(
+            self.state.clone(),
+            self.transport.clone(),
+            count,
+        )
+    }
+
+    /// `fetch_node_nodes` fetches nodes from a remote node.
+    pub fn fetch_node_nodes(
+        &mut self,
+        address: &[u8],
+        ids: &BTreeSet<Digest>,
+    ) -> Result<BTreeSet<Node>> {
+        protocol_network::fetch_node_nodes(self.state.clone(), self.transport.clone(), address, ids)
+    }
+
+    /// `fetch_nodes` fetches nodes from remote.
+    pub fn fetch_nodes(&mut self, ids: &BTreeSet<Digest>) -> Result<BTreeSet<Node>> {
+        protocol_network::fetch_nodes(self.state.clone(), self.transport.clone(), ids)
+    }
+
+    /// `fetch_node_random_nodes` fetches random nodes from a remote node.
+    pub fn fetch_node_random_nodes(
+        &mut self,
+        address: &[u8],
+        count: u32,
+    ) -> Result<BTreeSet<Node>> {
+        protocol_network::fetch_node_random_nodes(
+            self.state.clone(),
+            self.transport.clone(),
+            address,
+            count,
+        )
+    }
+
+    /// `fetch_random_nodes` fetches random nodes from remote.
+    pub fn fetch_random_nodes(&mut self, count: u32) -> Result<BTreeSet<Node>> {
+        protocol_network::fetch_random_nodes(self.state.clone(), self.transport.clone(), count)
+    }
+
+    /// `fetch_missing_ancestors` fetches a `Transaction` ancestors from remote if missing.
+    pub fn fetch_missing_ancestors(
+        &mut self,
+        transaction: &Transaction,
+    ) -> Result<BTreeSet<Transaction>> {
+        protocol_network::fetch_missing_ancestors(
+            self.state.clone(),
+            self.transport.clone(),
+            transaction,
+        )
+    }
+
+    /// `query_node` queries a single remote node.
+    pub fn query_node(&mut self, address: &[u8], transaction: &Transaction) -> Result<bool> {
+        protocol_network::query_node(
+            self.state.clone(),
+            self.transport.clone(),
+            address,
+            transaction,
+        )
+    }
+
+    /// `query` queries remote nodes.
+    pub fn query(&mut self, transaction: &Transaction) -> Result<u32> {
+        protocol_network::query(self.state.clone(), self.transport.clone(), transaction)
+    }
+
+    /// `mine` mines a set of `Transaction`s.
+    pub fn mine(&mut self, address: &[u8], transactions: &BTreeSet<Transaction>) -> Result<()> {
+        protocol_network::mine(
+            self.state.clone(),
+            self.transport.clone(),
+            address,
+            transactions,
+        )
+    }
+}
--- consensus/src/protocol/mod.rs
+++ consensus/src/protocol/mod.rs
@@ -3,10 +3,8 @@ pub mod network;
 pub mod state;
 pub use self::state::*;
 
-/*
 pub mod client;
 pub use self::client::*;
-*/
 
 pub mod server;
 pub use self::server::*;
