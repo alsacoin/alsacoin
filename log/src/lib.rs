f1c1fdc271fdf0fddedec557114c48449dc63a2a
chritchens <chritchens@gmail.com>
2019-09-07 12:09:05 +0200

    Add a log crate

---
+++ log/src/lib.rs
@@ -0,0 +1,12 @@
+//! # Log
+//!
+//! `log` contains Alsacoin`s logging types and functions.
+
+#[macro_use]
+extern crate failure;
+
+/// `error` contains the error type used in the crate.
+pub mod error;
+
+/// `result` contains the result type used in the crate.
+pub mod result;
