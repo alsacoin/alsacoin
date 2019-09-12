43614338472e02ebc2e272bb5400f41bc78c00eb
chritchens <chritchens@gmail.com>
2019-09-12 20:11:13 +0200

    Add error and result in cli

---
+++ cli/src/error.rs
@@ -0,0 +1,43 @@
+//! # Error
+//!
+//! `error` contains the `config` crate `Error` type.
+
+use serde_cbor;
+use serde_json;
+use std::convert::From;
+use std::io;
+
+#[derive(Debug, Fail)]
+pub enum Error {
+    #[fail(display = "IO: {}", msg)]
+    IO { msg: String },
+    #[fail(display = "Terminal: {}", msg)]
+    Terminal { msg: String },
+    #[fail(display = "Model: {}", msg)]
+    Model { msg: String },
+    #[fail(display = "Config: {}", msg)]
+    Config { msg: String },
+    #[fail(display = "Parse: {}", msg)]
+    Parse { msg: String },
+}
+
+impl From<io::Error> for Error {
+    fn from(error: io::Error) -> Error {
+        let msg = format!("{}", error);
+        Error::IO { msg }
+    }
+}
+
+impl From<serde_cbor::error::Error> for Error {
+    fn from(err: serde_cbor::error::Error) -> Error {
+        let msg = format!("{}", err);
+        Error::Parse { msg }
+    }
+}
+
+impl From<serde_json::error::Error> for Error {
+    fn from(err: serde_json::error::Error) -> Error {
+        let msg = format!("{}", err);
+        Error::Parse { msg }
+    }
+}
--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,3 +1,12 @@
 //! # CLI
 //!
 //! `cli` contains Alsacoin`s CLI types and functions.
+
+#[macro_use]
+extern crate failure;
+
+/// `error` contains the error type used in the crate.
+pub mod error;
+
+/// `result` contains the result type used in the crate.
+pub mod result;
